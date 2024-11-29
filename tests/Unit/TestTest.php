32227cb59ec439a49a0ed3327c06661894dc0a85
Kevin From <kevin@kevinfrom.dk>
2024-11-29 19:07:12 +0100

    init commit

---
+++ tests/Unit/TestTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kevinfrom\EconomicApi\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class TestTest extends TestCase
+{
+    public function testWillSucceed(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testWillFail(): void
+    {
+        $this->assertTrue(false);
+    }
+}
