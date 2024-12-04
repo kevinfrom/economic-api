8c4963fc7463dfe21354f4e4c68769e8b014b5e6
Kevin From <kevin@kevinfrom.dk>
2024-12-04 20:27:43 +0100

    adjusted VatZone class to match schema

--- src/Data/Entity/VatZoneEntity.php
+++ src/Data/Entity/VatZoneEntity.php
@@ -5,19 +5,18 @@ namespace Kevinfrom\EconomicApi\Data\Entity;
 final class VatZoneEntity
 {
     /**
-     * @param int $vatZoneNumber A unique identifier of the vat zone.
-     * @param string $name The name of the vat zone (max length: 50).
-     * @param bool $enabledForCustomer If true, the vat zone can be used for a customer.
-     * @param bool $enabledForSupplier If true, the vat zone can be used for a supplier.
-     * @param string $self A unique link reference to the vat zone item.
+     * @param string      $self A unique link reference to the vat zone item.
+     * @param int|null    $vatZoneNumber A unique identifier of the vat zone.
+     * @param string|null $name The name of the vat zone (max length: 50).
+     * @param bool|null   $enabledForCustomer If true, the vat zone can be used for a customer.
+     * @param bool|null   $enabledForSupplier If true, the vat zone can be used for a supplier.
      */
     public function __construct(
-        public int    $vatZoneNumber,
-        public string $name,
-        public bool   $enabledForCustomer,
-        public bool   $enabledForSupplier,
-        public string $self
-    )
-    {
+        public string $self,
+        public ?int $vatZoneNumber = null,
+        public ?string $name = null,
+        public ?bool $enabledForCustomer = null,
+        public ?bool $enabledForSupplier = null,
+    ) {
     }
 }
